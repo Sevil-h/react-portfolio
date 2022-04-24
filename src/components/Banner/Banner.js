e503e4bfed17c631f579892f20639d7463480bdd
Sevil Hatipoglu <sevilhatipogluu93@gmail.com>
2022-04-24 18:09:24 +0300

    added images and home page

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,31 @@
+import classes from "./banner.module.css";
+import { NavLink } from "react-router-dom";
+
+function Banner(props) {
+	return (
+		<div className={classes.container}>
+			<div className={classes.bigCircles}>
+				<div className={classes.bigCircle}></div>
+				<div className={classes.bigCircle}></div>
+				<div className={classes.bigCircle}></div>
+			</div>
+			<div className={classes.banner}>
+				<div className={classes.smallCircles}>
+					<div className={classes.smallCircle}></div>
+					<div className={classes.smallCircle}></div>
+					<div className={classes.smallCircle}></div>
+					<div className={classes.smallCircle}></div>
+					<div className={classes.smallCircle}></div>
+					<div className={classes.smallCircle}></div>
+				</div>
+				<h1 className={classes.title}>{props.title}</h1>
+				<h3 className={classes.subtitle}>{props.position}</h3>
+				<NavLink to="/projects">
+					<button className={classes.button}>View Portfolio</button>
+				</NavLink>
+			</div>
+		</div>
+	);
+}
+
+export default Banner;
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,10 @@
+import classes from "./layout.module.css";
+import React, { Fragment } from "react";
+
+import React from "react";
+
+function Layout(props) {
+	return <Fragment>{props.children}</Fragment>;
+}
+
+export default Layout;
