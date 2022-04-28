c2918ba18f4ed1a42918a1a9ed6721fe878ad517
Sevil Hatipoglu <sevilhatipogluu93@gmail.com>
2022-04-28 22:41:24 +0300

    remove unnecessary code

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,7 @@
 import React, { Fragment } from "react";
-import { useLocation } from "react-router-dom";
 import MainNavigation from "./MainNavigation";
-import classes from "./Layout.module.css";
 
 function Layout(props) {
-	const location = useLocation();
-	const path = location.pathname;
 	return (
 		<Fragment>
 			<MainNavigation />
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,5 +1,3 @@
-import { DiReact } from "react-icons/di";
-import { DiRubyRough } from "react-icons/di";
 import Cards from "../components/UI/Cards";
 
 import React from "react";
