5fc9fb4425bf27506952cf60bfea06861facfb68
Sevil Hatipoglu <sevilhatipogluu93@gmail.com>
2022-05-24 23:36:07 +0300

    -

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const NotFound = () => {
-	return <div>Page does not found!</div>;
+	return <div className="container">Page does not found!</div>;
 };
 
 export default NotFound;
